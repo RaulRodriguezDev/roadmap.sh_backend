79738131d9b91a9ad88a44b984c39411b83f4a56
Rodriguez, Raul Ivan <raulivan.rodriguez@solera.com>
2025-02-13 15:28:23 -0600

    Add new controller and model for blog posts

---
+++ BloggingPlatform/Controllers/BlogginPlatformController.cs
@@ -0,0 +1,51 @@
+﻿using BloggingPlatform.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloggingPlatform.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class BlogginPlatformController : Controller
+    {
+        [HttpPost]
+        [Route("posts")]
+        public IActionResult CreatePost(Post post)
+        {
+            // Create a new post
+            return Ok(post);
+        }
+
+        [HttpGet]
+        [Route("posts")]
+        public IActionResult GetPosts()
+        {
+            // Get all posts
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("posts/{id}")]
+        public IActionResult GetPost(int id)
+        {
+            // Get a post by id
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("posts/{id}")]
+        public IActionResult UpdatePost(int id, Post post)
+        {
+            // Update a post by id
+            return Ok(post);
+        }
+
+        [HttpDelete]
+        [Route("posts/{id}")]
+        public IActionResult DeletePost(int id)
+        {
+            // Delete a post by id
+            return Ok();
+        }
+
+    }
+}
---
+++ BloggingPlatform/Models/Post.cs
@@ -0,0 +1,13 @@
+﻿namespace BloggingPlatform.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Category { get; set; }
+        public string[] Tags { get; set; }
+        public DateOnly CreatedAt { get; set; }
+        public DateOnly UpdatedAt { get; set; }
+    }
+}
